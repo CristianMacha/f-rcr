c6dc81855f7a6613b95161e108a21d63e02ee515
cristianv.ms <73314207+CristianMacha@users.noreply.github.com>
2022-09-28 12:03:39 -0500

    skip one and two

--- src/app/pages/form/form.component.ts
+++ src/app/pages/form/form.component.ts
@@ -6,13 +6,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
+  imgPosition = {
+    left: '-120px',
+    right: '',
+    bottom: '0',
+    scale: 'scaleX(1)',
+  }
+
+  skipts = [
+    {
+      skip: 1,
+      active: false,
+    },
+    {
+      skip: 2,
+      active: true,
+    },
+    {
+      skip: 3,
+      active: false,
+    },
+    {
+      skip: 4,
+      active: false,
+    },
+    {
+      skip: 5,
+      active: false,
+    },
+    {
+      skip: 6,
+      active: false,
+    },
+    {
+      skip: 7,
+      active: false,
+    },
+    {
+      skip: 8,
+      active: false,
+    },
+    {
+      skip: 9,
+      active: false,
+    },
+    {
+      skip: 10,
+      active: false,
+    },
+  ];
 
   formRcr = {
     id: null,
     skipOne: {
       title: 'WHERE DO YOU NEED TILE?',
       subTitle: 'PICK ONE OR MANY AREAS',
-      selected: true,
+      selected: false,
       previusPage: null,
       nextPage: 2,
       options: [
@@ -51,33 +100,38 @@ export class FormComponent implements OnInit {
     skipTwo: {
       title: 'WHAT AREAS NEED TILING WORK?',
       subTitle: 'PICK ONE OR MANY AREAS',
-      selected: false,
+      selected: true,
       previusPage: 1,
       nextPage: 3,
       options: [
         {
           id: 1,
           name: 'FLOOR',
+          src: 'assets/imgs/Mesa_de_trabajo_16.jpg',
           selected: false,
         },
         {
           id: 2,
           name: 'BACKSPLASH',
+          src: 'assets/imgs/Mesa_de_trabajo_17.jpg',
           selected: false,
         },
         {
           id: 3,
           name: 'TUB/SHOWER ENCLOSURE',
+          src: 'assets/imgs/Mesa_de_trabajo_18.jpg',
           selected: false,
         },
         {
           id: 4,
           name: 'WALL',
+          src: 'assets/imgs/Mesa_de_trabajo_19.jpg',
           selected: false,
         },
         {
           id: 5,
           name: 'OTHER',
+          src: 'assets/imgs/LAPIZ.jpg',
           selected: false,
         }
       ]
@@ -191,6 +245,53 @@ export class FormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.setPositionImg();
   }
 
+  handleSelectOptionSkipOne(optionId: number) {
+    const index = this.formRcr.skipOne.options.findIndex((o) => o.id == optionId);
+    this.formRcr.skipOne.options[index].selected = !this.formRcr.skipOne.options[index].selected;
+  }
+
+  handlePreviousPage(previusPage: number) {
+    let skiptIndex = this.skipts.findIndex((s) => s.skip == previusPage);
+    this.skipts[skiptIndex].active = true;
+    const currentPage = ++previusPage;
+    let nextSkipIndex = this.skipts.findIndex((s) => s.skip == currentPage);
+    this.skipts[nextSkipIndex].active = false;
+
+    this.setPositionImg();
+  }
+
+  handleNextPage(nextPage: number) {
+    const previusPage = nextPage - 1;
+    let skiptIndex = this.skipts.findIndex((s) => s.skip == previusPage);
+    this.skipts[skiptIndex].active = false;
+
+    let nextSkipIndex = this.skipts.findIndex((s) => s.skip == nextPage);
+    this.skipts[nextSkipIndex].active = true;
+
+    this.setPositionImg();
+  }
+
+  setPositionImg() {
+    const index = this.skipts.findIndex((s) => s.active == true);
+    const skip = this.skipts[index];
+
+    if(skip.skip/2 == 1) {
+      this.imgPosition = {
+        right: '-120px',
+        bottom: '0px',
+        left: '',
+        scale: 'scaleX(-1)'
+      }
+    } else {
+      this.imgPosition = {
+        right: '0',
+        bottom: '0',
+        left: '-120px',
+        scale: 'scaleX(1)'
+      }
+    }
+  }
 }
